9042638ff7ea840c6816a863e91f3a1cd4fed3fa
kyminbb <kyminbb.kim@gmail.com>
2019-06-07 01:41:39 +0900

    (190606) Roughly designed the sign-up page.

--- app/src/main/java/com/kyminbb/militarycalendar/MainActivity.kt
+++ app/src/main/java/com/kyminbb/militarycalendar/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.kyminbb.militarycalendar
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.jakewharton.threetenabp.AndroidThreeTen
+import org.jetbrains.anko.startActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Initialize the timezone information.
+        AndroidThreeTen.init(this)
         setContentView(R.layout.activity_main)
+
+        val prefs = getSharedPreferences("prefs", Context.MODE_PRIVATE)
+        val firstStart = prefs.getBoolean("firstStart", true)
+
+        // Opens the sign-up page if the application is first-time executed.
+        if (firstStart) {
+            startActivity<AccountActivity>()
+        }
     }
 }
---
+++ app/src/main/java/com/kyminbb/militarycalendar/User.kt
@@ -0,0 +1,14 @@
+package com.kyminbb.militarycalendar
+
+import com.commit451.addendum.threetenabp.toLocalDate
+import org.threeten.bp.LocalDate
+import java.util.*
+
+// Initialize the user info
+data class User(
+    var name: String = "",
+    var affiliation: String = "육군",
+    var profileImage: String = "",
+    var rank: Int = 0,
+    var promotionDates: MutableList<LocalDate> = MutableList(5, { Calendar.getInstance().toLocalDate() })
+)
