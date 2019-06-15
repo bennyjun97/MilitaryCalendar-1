ab649c98d392d7e13e2a1500a8bd1084dd6a128d
eriehan <eriehan@gmail.com>
2019-06-15 23:59:46 +0900

    calculation of ETS date added

--- app/src/main/java/com/kyminbb/militarycalendar/SettingActivity.kt
+++ app/src/main/java/com/kyminbb/militarycalendar/SettingActivity.kt
@@ -255,6 +255,7 @@ class SettingActivity : AppCompatActivity() {
     }
 
     private fun setEndDate() {
+        //call calcETS method of dateCalc to calculate ETS date
         userInfo.promotionDates[Dates.END.ordinal] = dateCalc.calcETS(userInfo.promotionDates[Dates.ENLIST.ordinal], userInfo.affiliation)
         inputEndDate.text = formatDate(userInfo.promotionDates[Dates.END.ordinal])
     }
