e686a7b659b359194abf93f67494edbe733da384
Hyungbin Jun <wjsgudqls97@gmail.com>
2019-06-20 02:52:26 +0900

    more graphics

--- app/src/main/java/com/kyminbb/militarycalendar/ClockActivity.kt
+++ app/src/main/java/com/kyminbb/militarycalendar/ClockActivity.kt
@@ -1,13 +1,14 @@
 package com.kyminbb.militarycalendar
 
 import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
+
 import kotlinx.android.synthetic.main.activity_clock.*
 
 class ClockActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_clock)
     }
 
--- app/src/main/java/com/kyminbb/militarycalendar/MainActivity.kt
+++ app/src/main/java/com/kyminbb/militarycalendar/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.kyminbb.militarycalendar
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.jakewharton.threetenabp.AndroidThreeTen
@@ -17,9 +18,11 @@ class MainActivity : AppCompatActivity() {
         val prefs = getSharedPreferences("prefs", Context.MODE_PRIVATE)
         val firstStart = prefs.getBoolean("firstStart", true)
 
+        startActivity<TabActivity>()
+
         // Open the sign-up page if the application is first-time executed.
         // if (firststart)이어야 하나 테스트를 위해 if(true)로 임시 설정함
-        if (true) {
+        if (false) {
             startActivity<SetNameActivity>()
             //startActivity<SettingNameActivity>()
         }
