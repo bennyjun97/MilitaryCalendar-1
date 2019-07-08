63932308c6182712edf0cf802970adda0b1d3031
Hyungbin Jun <wjsgudqls97@gmail.com>
2019-07-08 15:16:45 +0900

    added SetTestActivity

---
+++ app/src/main/java/com/kyminbb/militarycalendar/activities/register/SetTestActivity.kt
@@ -0,0 +1,13 @@
+package com.kyminbb.militarycalendar.activities.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kyminbb.militarycalendar.R
+
+class SetTestActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_set_test)
+    }
+}
