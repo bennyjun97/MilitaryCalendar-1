83c245392e97b208f91ff10257b656e42995cc60
Hyungbin Jun <wjsgudqls97@gmail.com>
2019-07-12 22:48:33 +0900

    calendar add button added

--- app/src/main/java/com/kyminbb/militarycalendar/activities/main/CalendarFragment.kt
+++ app/src/main/java/com/kyminbb/militarycalendar/activities/main/CalendarFragment.kt
@@ -14,6 +14,7 @@ import kotlinx.android.synthetic.main.fragment_calendar.*
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
+private var adding = true
 /**
  * A simple [Fragment] subclass.
  *
@@ -25,9 +26,35 @@ class CalendarFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_calendar, container, false)
+    }
 
+    // Update UI after views are created.
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        buttonAdd.setOnClickListener{
+            if(adding) {
+                addTab.setVisibility(View.VISIBLE)
+            }
+            else{
+                addTab.setVisibility(View.GONE)
+            }
+            adding = !adding
+        }
+
+        addLeave.setOnClickListener{
+            testText.text = "휴가 추가"
+        }
+        addDuty.setOnClickListener{
+            testText.text = "당직 추가"
+        }
+        addExercise.setOnClickListener{
+            testText.text = "훈련 추가"
+        }
+        addPersonal.setOnClickListener{
+            testText.text = "개인일정 추가"
+        }
     }
 }
