3610b490116944da95063b5bcff89792e6e33ce8
kyminbb <kyminbb.kim@gmail.com>
2019-07-14 00:21:19 -0400

    Implemented a basic database structure.

--- app/src/main/java/com/kyminbb/militarycalendar/activities/main/CalendarFragment.kt
+++ app/src/main/java/com/kyminbb/militarycalendar/activities/main/CalendarFragment.kt
@@ -1,14 +1,17 @@
 package com.kyminbb.militarycalendar.activities.main
 
+import android.content.ContentValues
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.BaseColumns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.fragment.app.Fragment
 import com.kyminbb.militarycalendar.R
 import com.kyminbb.militarycalendar.database.DBHelper
+import com.kyminbb.militarycalendar.database.TableReaderContract
 import kotlinx.android.synthetic.main.fragment_calendar.*
+import org.jetbrains.anko.support.v4.toast
 
 
 class CalendarFragment : Fragment() {
@@ -19,7 +22,6 @@ class CalendarFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        loadDB()
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_calendar, container, false)
     }
@@ -28,34 +30,88 @@ class CalendarFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        buttonAdd.setOnClickListener{
-            if(adding) {
-                addTab.setVisibility(View.VISIBLE)
-            }
-            else{
-                addTab.setVisibility(View.GONE)
+        val dbHelper = DBHelper(this.context!!)
+
+        buttonAdd.setOnClickListener {
+            if (adding) {
+                addTab.visibility = View.VISIBLE
+            } else {
+                addTab.visibility = View.GONE
             }
             adding = !adding
         }
 
-        addLeave.setOnClickListener{
-            testText.text = "휴가 추가"
-        }
-        addDuty.setOnClickListener{
-            testText.text = "당직 추가"
-        }
-        addExercise.setOnClickListener{
-            testText.text = "훈련 추가"
-        }
-        addPersonal.setOnClickListener{
-            testText.text = "개인일정 추가"
-        }
+        calendarView.setOnDateChangeListener { _, year, month, dayOfMonth ->
+            val date = formatDate(year, month+1, dayOfMonth)
 
-        // For testing database management
+            // Show saved schedules of the date.
+            val contents = readDB(dbHelper, date)
+            toast("$contents")
 
+            // Add a schedule on the date.
+            val addButtons = mapOf(addLeave to "휴가", addDuty to "당직", addExercise to "훈련", addPersonal to "개인일정")
+            for (button in addButtons.keys) {
+                button.setOnClickListener {
+                    writeDB(dbHelper, (addButtons[button])!!, date)
+                    val text = "${addButtons[button]} 추가"
+                    testText.text = text
+                }
+            }
+        }
     }
 
-    private fun loadDB() {
-        val db = DBHelper(this.context!!)
+    private fun writeDB(dbHelper: DBHelper, content: String, date: String) {
+        val db = dbHelper.writableDatabase
+
+        val values = ContentValues().apply {
+            put(TableReaderContract.TableEntry.COLUMN_START_DATE, date)
+            put(TableReaderContract.TableEntry.COLUMN_END_DATE, date)
+            put(TableReaderContract.TableEntry.COLUMN_CONTENT, content)
+        }
+        db?.insert(TableReaderContract.TableEntry.TABLE_NAME, null, values)
+    }
+
+    private fun readDB(dbHelper: DBHelper, date: String): MutableList<String> {
+        val db = dbHelper.readableDatabase
+
+        // Define a projection that specifies which columns from the database you will actually find after the query.
+        val projection = arrayOf(
+            BaseColumns._ID,
+            TableReaderContract.TableEntry.COLUMN_START_DATE,
+            TableReaderContract.TableEntry.COLUMN_END_DATE,
+            TableReaderContract.TableEntry.COLUMN_CONTENT
+        )
+
+        val selection = "${TableReaderContract.TableEntry.COLUMN_START_DATE} = ?"
+        val selectionArgs = arrayOf(date)
+
+        val sortOrder = "${TableReaderContract.TableEntry.COLUMN_END_DATE} ASC"
+
+        val cursor = db.query(
+            TableReaderContract.TableEntry.TABLE_NAME,
+            projection,
+            selection,
+            selectionArgs,
+            null,
+            null,
+            sortOrder
+        )
+
+        val contents = mutableListOf<String>()
+        // Read the column's value by iterating through results.
+        with(cursor) {
+            while (moveToNext()) {
+                val content = getString(getColumnIndexOrThrow(TableReaderContract.TableEntry.COLUMN_CONTENT))
+                if (content != null) {
+                    contents.add(content)
+                }
+            }
+        }
+        cursor.close()
+        return contents
+    }
+
+    private fun formatDate(year: Int, month: Int, dayOfMonth: Int): String {
+        return "$year-$month-$dayOfMonth"
     }
 }
--- app/src/main/java/com/kyminbb/militarycalendar/database/DBHelper.kt
+++ app/src/main/java/com/kyminbb/militarycalendar/database/DBHelper.kt
@@ -6,8 +6,9 @@ import android.database.sqlite.SQLiteOpenHelper
 import android.provider.BaseColumns
 
 
+// Define the schema of the database.
 object TableReaderContract {
-    object TableReader : BaseColumns {
+    object TableEntry : BaseColumns {
         const val TABLE_NAME = "calendar"
         const val COLUMN_START_DATE = "start_date"
         const val COLUMN_END_DATE = "end_date"
@@ -16,13 +17,13 @@ object TableReaderContract {
 }
 
 private const val SQL_CREATE_ENTRIES =
-    "CREATE TABLE ${TableReaderContract.TableReader.TABLE_NAME} (" +
+    "CREATE TABLE ${TableReaderContract.TableEntry.TABLE_NAME} (" +
             "${BaseColumns._ID} INTEGER_PRIMARY_KEY," +
-            "${TableReaderContract.TableReader.COLUMN_START_DATE} TEXT" +
-            "${TableReaderContract.TableReader.COLUMN_END_DATE} TEXT" +
-            "${TableReaderContract.TableReader.COLUMN_CONTENT} TEXT)"
+            "${TableReaderContract.TableEntry.COLUMN_START_DATE} TEXT," +
+            "${TableReaderContract.TableEntry.COLUMN_END_DATE} TEXT," +
+            "${TableReaderContract.TableEntry.COLUMN_CONTENT} TEXT)"
 
-private const val SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS ${TableReaderContract.TableReader.TABLE_NAME}"
+private const val SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS ${TableReaderContract.TableEntry.TABLE_NAME}"
 
 
 class DBHelper(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, 1) {
